0430cc4e57cbb360e9211f640d04bc00d4df2ea2
Omkottewar <omkottewar19.04@gmail.com>
2024-10-24 17:03:10 +0530

    created folder structure

---
+++ src/Components/FilterChat/FilterChat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterChat = () => {
+  return (
+    <div>FilterChat</div>
+  )
+}
+
+export default FilterChat
---
+++ src/Components/StartingChat/StartingChat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StartingChat = () => {
+  return (
+    <div>StartingChat</div>
+  )
+}
+
+export default StartingChat
